75ef8293ec5723f4035ad360c607f09eab717606
Seth David Gyimah <esec26@gmail.com>
2022-10-18 11:40:43 +0000

    completed code along requirements

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
   
   def index
+
+    byebug
+    #setting the value of session hash
     session[:session_hello] ||= "World"
+
+    #setting the value of cookies hash
     cookies[:cookies_hello] ||= "World"
+
+    # serializing or rendering value of "session" and "cookies" in a hash format
     render json: { session: session, cookies: cookies.to_hash }
   end
 
